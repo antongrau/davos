59cedd5f69ef27623226e0866128e90690c9b440
Anton Grau Larsen <antongrau@gmail.com>
2020-01-30 09:39:15 +0100

    Upload of data

---
+++ R/Data documentation.R
@@ -0,0 +1,6 @@
+#' Davos dataset
+#'
+#'Lists of participants of the World Economic Forum from 2013-2019 period.
+#'@details The data originally was originally published by Quartz and has been assembled and cleaned with a focus on the organizations.
+#'
+#'@docType data
